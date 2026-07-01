7e07337be2f41a840fd77f14c1b961b9da174196
mrityunjaybhagat <mrityunjaybhagat@gmail.com>
2026-07-01 11:28:49 +0000

    feat: add private route table and subnet association

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -69,3 +69,18 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.name_prefix}-private-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
